e5922bd2ee2dab4b4ce5cdb97306424a4a7b912d
nicodev-git <schaefernicker@gmail.com>
2021-06-10 22:39:47 +0300

    rendering data

--- index.js
+++ index.js
@@ -17,12 +17,25 @@ app.use(express.static("public"));
 
 app.get("/", (req, res, next) => {
   //   res.sendFile(path.join(__dirname, "views", "index.html"));
-  res.render("index");
+  res.render("index", {
+    title: "Main page",
+    isHome: true,
+  });
 });
 
-app.get("/about", (req, res) => {
+app.get("/add", (req, res) => {
   //   res.sendFile(path.join(__dirname, "views", "about.html"));
-  res.render("about");
+  res.render("add", {
+    title: "add course",
+    isAdd: true,
+  });
+});
+app.get("/courses", (req, res) => {
+  //   res.sendFile(path.join(__dirname, "views", "about.html"));
+  res.render("courses", {
+    title: "Courses",
+    isCourses: true,
+  });
 });
 
 const PORT = process.env.PORT || 3000;
