ccb3dd3003f55c59c0c157ca0be874b5ec6e4a98
nicodev-git <schaefernicker@gmail.com>
2021-06-11 15:46:29 +0300

    edit course on person side

--- models/course.js
+++ models/course.js
@@ -18,6 +18,28 @@ class Course {
       id: this.id,
     };
   }
+
+  static async update(course) {
+    const courses = await Course.getAll();
+
+    const idx = courses.findIndex((c) => c.id === course.id);
+    courses[idx] = course;
+
+    return new Promise((resolve, reject) => {
+      fs.writeFile(
+        path.join(__dirname, "..", "data", "courses.json"),
+        JSON.stringify(courses),
+        (err) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve();
+          }
+        }
+      );
+    });
+  }
+
   async save() {
     const courses = await Course.getAll();
     courses.push(this.toJSON());
@@ -51,6 +73,10 @@ class Course {
         }
       );
     });
+  }
+  static async getById(id) {
+    const courses = await Course.getAll();
+    return courses.find((c) => c.id === id);
   }
 }
 module.exports = Course;
---
+++ public/app.js
@@ -0,0 +1,6 @@
+document.querySelectorAll(".price").forEach((node) => {
+  node.textContent = new Intl.NumberFormat("ru-RU", {
+    currency: "UAH",
+    style: "currency",
+  }).format(node.textContent);
+});
