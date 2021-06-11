227da37e3212a64995be471f2b32f03d8a51911d
nicodev-git <schaefernicker@gmail.com>
2021-06-11 16:56:52 +0300

    card, add to card

---
+++ models/card.js
@@ -0,0 +1,54 @@
+const path = require("path");
+const fs = require("fs");
+const { resolve } = require("path");
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  "data",
+  "card.json"
+);
+
+class Card {
+  static async add(course) {
+    const card = await Card.fetch();
+
+    const idx = card.courses.findIndex((c) => c.id === course.id);
+
+    const candidate = card.courses[idx];
+
+    if (candidate) {
+      //course is here already
+      candidate.count++;
+      card.courses[idx] = candidate;
+    } else {
+      //need to add
+      course.count = 1;
+      card.courses.push(course);
+    }
+
+    card.price += +course.price;
+
+    return new Promise((resolve, reject) => {
+      fs.writeFile(p, JSON.stringify(card), (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      });
+    });
+  }
+
+  static async fetch() {
+    return new Promise((resolve, reject) => {
+      fs.readFile(p, "utf-8", (err, content) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(JSON.parse(content));
+        }
+      });
+    });
+  }
+}
+module.exports = Card;
