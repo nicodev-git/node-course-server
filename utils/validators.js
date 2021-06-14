fe2909d02c99b83d53903ec89862c58ae59bca92
nicodev-git <schaefernicker@gmail.com>
2021-06-14 21:56:48 +0300

    validation sanitizers

--- utils/validators.js
+++ utils/validators.js
@@ -13,17 +13,22 @@ exports.registerValidators = [
       } catch (e) {
         console.log(e);
       }
-    }),
+    })
+    .normalizeEmail(),
   body("password", "Password must be minimus 4 symbols")
     .isLength({ min: 4, max: 56 })
-    .isAlphanumeric(),
-  body("confirm").custom((value, { req }) => {
-    if (value !== req.body.password) {
-      throw new Error("Passwords must be the same");
-    }
-    return true;
-  }),
+    .isAlphanumeric()
+    .trim(),
+  body("confirm")
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Passwords must be the same");
+      }
+      return true;
+    })
+    .trim(),
   body("name")
     .isLength({ min: 3 })
-    .withMessage("the name must be bigger than 3 letters"),
+    .withMessage("the name must be bigger than 3 letters")
+    .trim(),
 ];
